86138eb81994233de6d44cf6064c65a80af00943
yangyh <yangyh0609@cairenhui.com>
2019-04-22 09:54:36 +0800

    day07-code:常用API 匿名对象

---
+++ day07-code/src/com/yangyh/day07/demo02/Person.java
@@ -0,0 +1,15 @@
+package com.yangyh.day07.demo02;
+
+/**
+ * @description: 匿名对象的使用
+ * @author: yangyh
+ * @create: 2019-04-21 21:41
+ **/
+public class Person {
+
+    String name;
+
+    public void show(){
+        System.out.println(this.name);
+    }
+}
