aec87d690a1cbe56bd04c8c253a916c5f2a9e594
yangyh <yangyh0609@cairenhui.com>
2019-04-22 09:54:13 +0800

    day07-code:常用API Scanner类

---
+++ day07-code/src/com/yangyh/day07/demo01/Demo02ScannerSum.java
@@ -0,0 +1,33 @@
+package com.yangyh.day07.demo01;
+
+import java.util.Scanner;
+
+/**
+ * @description: 键盘出入两个数字求和
+ * @author: yangyh
+ * @create: 2019-04-20 18:00
+ *
+ * 题目：键盘输入两个数字，求出和值
+ *
+ * 思路：
+ * 1.既然是键盘输入，那么就用Scanner。
+ * 2.Scanner的三个步骤：导包、创建、使用。
+ * 3.需要的是两个数字，所以需要调用两次nextInt()方法。
+ * 4.得到了两个数字，就需要加在一起。
+ * 5.将结果打印。
+ **/
+public class Demo02ScannerSum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入第一个数字：");
+        int num1 = sc.nextInt();
+
+        System.out.println("请输入第二个数字：");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("两个数字的和为：" + sum);
+    }
+}
