a8322ee8853bff2c7ac3223eddea64ffa267ad5d
yangyh <yangyh0609@cairenhui.com>
2019-05-07 19:38:14 +0800

    day11-code:权限修饰符

---
+++ code11-code/src/com/yangyh/day11/demo02/sub/MySon.java
@@ -0,0 +1,16 @@
+package com.yangyh.day11.demo02.sub;
+
+
+import com.yangyh.day11.demo02.MyClass;
+
+/**
+ * @description:
+ * @author: yangyh
+ * @create: 2019-05-07 19:33
+ **/
+public class MySon extends MyClass {
+
+    public void methodSon() {
+        System.out.println(super.num);
+    }
+}
