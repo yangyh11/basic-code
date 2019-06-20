139268af86d64becc07ddbdfa843fd9cfdfccc90
yangyh <yangyh11@163.com>
2019-06-20 23:20:07 +0800

    day13-code:Collections集合工具类排序

---
+++ day13-code/src/com/yangyh/day13/demo08/collections/Student.java
@@ -0,0 +1,44 @@
+package com.yangyh.day13.demo08.collections;
+
+/**
+ * @description:
+ * @author: yangyh
+ * @create: 2019-06-17 22:32
+ */
+public class Student {
+
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
