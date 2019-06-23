7ac06d26591ae476c75b69662cd4ba237fa020d1
yangyh <yangyh11@163.com>
2019-06-23 19:50:07 +0800

    day13-code:Map集合中常用的方法

--- day13-code/src/com/yangyh/day13/demo09/map/Demo01Map.java
+++ day13-code/src/com/yangyh/day13/demo09/map/Demo01Map.java
@@ -14,6 +14,11 @@ import java.util.Map;
  *      2.Map集合中的元素，key和value的数据类型可以相同，也可以不同。
  *      3.Map中的元素，key是不允许重复的，value是可以重复的。
  *      4.Map集合中的元素，key和value是一一对应的。
+ * Map集合中常用的方法：
+ *      1.V put(K key, V value) // 把指定的键与制定的值添加到Map集合中。
+ *      2.V remove(Object key) // 把指定的键对应的键值对元素在Map集合中删除，返回被删除元素的值。
+ *      3.V get(Object key)     // 根据指定的键，在Map集合中获取对应的值。
+ *      4.boolean containsKey(Object key) // 判断集合中是否包含指定的键
  * java.util.HashMap<k,v>集合 implements Map<K,V>接口
  * HashMap集合的特点：
  *      1.HashMap集合底层是哈希表：查询速度非常快
@@ -27,7 +32,7 @@ import java.util.Map;
  */
 public class Demo01Map {
     public static void main(String[] args) {
-        method02();
+        method04();
     }
 
     /**
@@ -64,13 +69,51 @@ public class Demo01Map {
         map.put("林志玲", 178);
 
         Integer v1 = map.remove("林志玲");
-        System.out.println(v1);
-        System.out.println(map);
+        System.out.println(v1); // 178
+        System.out.println(map); // {赵丽颖=168, 杨颖=165}
 
         // 必须用Integer接收，用int接收会报错
         Integer v2 = map.remove("古力娜扎");
-        System.out.println(v2);
-        System.out.println(map);
+        System.out.println(v2); // null
+        System.out.println(map); // {赵丽颖=168, 杨颖=165}
+    }
+
+    /**
+     * V get(Object key) // 根据指定的键，在Map集合中获取对应的值。
+     *      返回值：V
+     *          key存在，返回对应value值。
+     *          key不存在，返回null
+     */
+    private static void method03() {
+        // 创建Map集合对象
+        Map<String, Integer> map = new HashMap<>();
+        map.put("赵丽颖", 168);
+        map.put("杨颖", 165);
+        map.put("林志玲", 178);
+
+        Integer v1 = map.get("杨颖");
+        System.out.println(v1); // 165
+
+        Integer v2 = map.get("古力娜扎");
+        System.out.println(v2); // null
+    }
+
+    /**
+     * boolean containsKey(Object key) // 判断集合中是否包含指定的键
+     * 包含返回true，不包含返回false
+     */
+    private static void method04() {
+        // 创建Map集合对象
+        Map<String, Integer> map = new HashMap<>();
+        map.put("赵丽颖", 168);
+        map.put("杨颖", 165);
+        map.put("林志玲", 178);
+
+        boolean b1 = map.containsKey("杨颖");
+        System.out.println(b1); // true
+
+        boolean b2 = map.containsKey("古力娜扎");
+        System.out.println(b2); // false
     }
 
 }
